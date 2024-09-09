22d135eee1cad5317d2cb183252b3fc906b60983
Caleb Furley <caleb.furley@gmail.com>
2024-09-08 23:19:41 -0500

    Organize Project

---
+++ concepts/searching/interpolation.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int interpolationSearch(int array[], size_t size, int key)
+{
+  //searching..
+  return -1;
+}
+
+int main(int argx, char** argv)
+{
+  int array[] = {6,4,9,6,3,1,2,7,8};
+  int size = 9;
+  int key = 1;
+
+  cout << "Found at Index = " << interpolationSearch(array, size, key) << endl;
+  return 0;
+}
