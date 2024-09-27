f9e8388ce5c33dcb6e27b0578dd2a546e3072e16
Caleb Furley <caleb.furley@gmail.com>
2024-09-27 16:51:49 -0500

    Sep27 Class

---
+++ inclass/sep27.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() { 
+  return 0;
+}
+
+/*
+  For linked list:
+    1. write a copy constructor (LinkedList<DT>* other)
+    2. write a overloaded = operator for deep copy.
+*/
